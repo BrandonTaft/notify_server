6aa9dc481af970e3638f748f718b71e9611e84f6
Brandon Taft <btaftcan@gmail.com>
2024-03-08 19:37:26 -0500

    eod

--- server.js
+++ server.js
@@ -45,52 +45,47 @@ let chatRooms = [
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
-  socket.on("createRoom", (roomName) => {
-      socket.join(roomName);
+  socket.on("createRoom", (name) => {
+      socket.join(name);
       //👇🏻 Adds the new group name to the chat rooms array
-      chatRooms.unshift({ id: generateID(), roomName, messages: [] });
+      chatRooms.unshift({ id: generateID(), name, messages: [] });
       //👇🏻 Returns the updated chat rooms via another event
       socket.emit("roomsList", chatRooms);
   });
+
+  socket.on("findRoom", (id) => {
+    //👇🏻 Filters the array by the ID
+    let result = chatRooms.filter((room) => room.id == id);
+    //👇🏻 Sends the messages to the app
+    socket.emit("foundRoom", result[0].messages);
+  });
+
+  socket.on("newMessage", (data) => {
+		const { room_id, message, user, timestamp } = data;
+		let result = chatRooms.filter((room) => room.id == room_id);
+		const newMessage = {
+			id: generateID(),
+			text: message,
+			user,
+			time: `${timestamp.hour}:${timestamp.mins}`,
+		};
+		console.log("New Message", newMessage);
+		socket.to(result[0].name).emit("roomMessage", newMessage);
+		result[0].messages.push(newMessage);
+
+		socket.emit("roomsList", chatRooms);
+    console.log("foundRoom", result[0].messages)
+		socket.emit("foundRoom", result[0].messages);
+	});
 
   socket.on("disconnect", () => {
       socket.disconnect();
       console.log("🔥: A user disconnected");
   });
-
-
-socket.on("findRoom", (id) => {
-  //👇🏻 Filters the array by the ID
-  let result = chatRooms.filter((room) => room.id == id);
-  //👇🏻 Sends the messages to the app
-  socket.emit("foundRoom", result[0].messages);
-});
-
-socket.on("newMessage", (data) => {
-  //👇🏻 Destructures the property from the object
-  const { room_id, message, user, timestamp } = data;
-
-  //👇🏻 Finds the room where the message was sent
-  let result = chatRooms.filter((room) => room.id == room_id);
-
-  //👇🏻 Create the data structure for the message
-  const newMessage = {
-      id: generateID(),
-      text: message,
-      user,
-      time: `${timestamp.hour}:${timestamp.mins}`,
-  };
-  //👇🏻 Updates the chatroom messages
-  socket.to(result[0].name).emit("roomMessage", newMessage);
-  result[0].messages.push(newMessage);
-
-  //👇🏻 Trigger the events to reflect the new changes
-  socket.emit("roomsList", chatRooms);
-  socket.emit("foundRoom", result[0].messages);
-});
 });
 
 app.get("/api", (req, res) => {
+  console.log('chatrooms', chatRooms)
   res.json(chatRooms);
 });
 
@@ -132,4 +127,7 @@ app.use(function (err, req, res, next) {
   }
 });
 
-module.exports = app;
+// module.exports = app;
+http.listen(config.APP_PORT, () => {
+	console.log(`Server listening on ${config.APP_PORT}`);
+});
