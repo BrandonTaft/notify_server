a4726755b95f0e5713ee6cbef98c4baa59df1026
Brandon Taft <btaftcan@gmail.com>
2024-04-03 20:42:42 -0400

    eod

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const multer = require('multer');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors')
@@ -24,13 +25,15 @@ let chatRooms = [];
 socketIO.on("connection", (socket) => {
   console.log(`⚡: ${socket.id} user just connected!`);
 
-  socket.on("createRoom", ({name, organization}) => {
-    console.log("ORG : ", organization)
+  socket.on("createRoom", ({id,name, organization}) => {
+    
       socket.join(name);
-      //👇🏻 Adds the new group name to the chat rooms array
-      chatRooms.unshift({ id: generateID(), name, messages: [] });
-      //👇🏻 Returns the updated chat rooms via another event
-      socketIO.emit("roomsList", chatRooms);
+      
+      chatRooms.unshift({ id, organization, name, messages: [] });
+     
+      const authorizedChatRooms =  chatRooms.filter(chatRoom => chatRoom.organization === organization)
+      
+      socketIO.emit("chatRoomList", authorizedChatRooms);
   });
 
   socket.on("findRoom", (id) => {
@@ -41,17 +44,43 @@ socketIO.on("connection", (socket) => {
   });
 
   socket.on("newMessage", (data) => {
-		const { room_id, message, user, timestamp } = data;
+    console.log("GOT NEW MESSAGE")
+		const { room_id, message, user, reactions, timestamp } = data;
 		let result = chatRooms.filter((room) => room.id == room_id);
 		const newMessage = {
 			id: generateID(),
+      room_id,
 			text: message,
 			user,
 			time: `${timestamp.hour}:${timestamp.mins}`,
+      reactions
 		};
 		result[0].messages.push(newMessage);
-		socketIO.emit("roomsList", chatRooms);
+		socketIO.emit("chatRoomList", chatRooms);
 		socketIO.emit("newMessage", result[0].messages);
+	});
+
+  socket.on("newReaction", (data) => {
+    console.log("GOT NEW Reaction", data)
+		const { room_id, id, reaction } = data;
+		let room = chatRooms.filter((room) => room.id == room_id);
+    let message = room[0].messages.filter((message) => message.id === id )
+    if (message[0]) {
+      message[0].reactions[reaction]++
+    }
+  
+		// const newMessage = {
+		// 	id: generateID(),
+    //   room_id,
+		// 	text: message,
+		// 	user,
+		// 	time: `${timestamp.hour}:${timestamp.mins}`,
+    //   reactions
+		// };
+		// result[0].messages.push(newMessage);
+		 socketIO.emit("newReaction", message[0].reactions);
+    console.log("RESULTS", message[0])
+    console.log("CHATROOMS", chatRooms[0].messages)
 	});
 
   socket.on("disconnect", () => {
@@ -60,7 +89,7 @@ socketIO.on("connection", (socket) => {
   });
 });
 
-app.get("/api", (req, res) => {
+app.get("/chatrooms", (req, res) => {
   console.log('chatrooms', chatRooms)
   res.json(chatRooms);
 });
@@ -73,7 +102,6 @@ mongoose.connect(config.DB, {
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
@@ -82,12 +110,27 @@ app.use(
 );
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-
-
-
-
 app.use('/', reminders);
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, './public/images');
+  },
+  filename(req, file, callback) {
+    console.log("REQQQ",file)
+    callback(null, `${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post('/api/profile-image', upload.single('photo'), (req, res) => {
+  console.log('file', req.file);
+  console.log('body', req.body.userId);
+  res.status(200).json({
+    message: 'success!',
+  });
+});
 
 //******Catch 404 Errors And Forward To Error Handler*****//
 app.use(function (req, res, next) {
